9de28d759808409bef92d0faf246339a31302037
coeduca <coeducaanimas@gmail.com>
2026-03-26 08:49:49 -0600

    Update students_8A.js

--- data/students_8A.js
+++ data/students_8A.js
@@ -3,21 +3,21 @@
 // Para agregar/editar estudiantes, modifica este arreglo.
 // ============================================================
 (window.STUDENTS = window.STUDENTS || []).push(
-  { nie: "20176655", nombre: "Ángel Martínez, Génesis Guadalupe",      grado: "8° Grado A", nacimiento: "21/11/2011", sexo: "F", demeritos_url: "" },
-  { nie: "20176657", nombre: "Barrera Hernández, Emerson Geremy",      grado: "8° Grado A", nacimiento: "24/06/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20176656", nombre: "Barrera Hernández, Karen Melany",        grado: "8° Grado A", nacimiento: "27/09/2011", sexo: "F", demeritos_url: "" },
-  { nie: "20176661", nombre: "Corvera Alfaro, Gustavo Ezequiel",       grado: "8° Grado A", nacimiento: "12/03/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20138869", nombre: "Corvera De Paz, Diego Benjamín",         grado: "8° Grado A", nacimiento: "27/08/2011", sexo: "M", demeritos_url: "" },
-  { nie: "20176662", nombre: "Corvera Flores, Pedro Ernesto",          grado: "8° Grado A", nacimiento: "04/09/2011", sexo: "M", demeritos_url: "" },
-  { nie: "20176663", nombre: "Corvera Fuentes, Luis Rafael",           grado: "8° Grado A", nacimiento: "11/09/2011", sexo: "M", demeritos_url: "" },
-  { nie: "10065035", nombre: "Duran Gómez, Justin Leonel",             grado: "8° Grado A", nacimiento: "29/12/2011", sexo: "M", demeritos_url: "" },
-  { nie: "20176664", nombre: "González Corvera, Héctor Gabriel",       grado: "8° Grado A", nacimiento: "02/02/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20305579", nombre: "Henríquez Chirino, Estiven Alexander",   grado: "8° Grado A", nacimiento: "15/06/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20305581", nombre: "Manueles Pereira, Keily Yaritza",        grado: "8° Grado A", nacimiento: "05/07/2011", sexo: "F", demeritos_url: "" },
-  { nie: "20176667", nombre: "Martínez Flores, Gisela Lisbeth",        grado: "8° Grado A", nacimiento: "14/03/2012", sexo: "F", demeritos_url: "" },
+  { nie: "20176655", nombre: "Ángel Martínez, Génesis Guadalupe",      grado: "8° Grado A", nacimiento: "21/11/2011", sexo: "F", demeritos_url: "https://docs.google.com/spreadsheets/d/1yVtho-RuTkayyM8CHTbR5nBoVEV8KvDenHnXaBOBNkU/edit?usp=sharing" },
+  { nie: "20176657", nombre: "Barrera Hernández, Emerson Geremy",      grado: "8° Grado A", nacimiento: "24/06/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1bezLCFTeFsBLzaYZwqY13Ky3C0XMh4Fcr9UmmVT8DTE/edit?usp=sharing" },
+  { nie: "20176656", nombre: "Barrera Hernández, Karen Melany",        grado: "8° Grado A", nacimiento: "27/09/2011", sexo: "F", demeritos_url: "https://docs.google.com/spreadsheets/d/1q0zfrl1iK2ayqH-MrjIj6LHwMfSycHBqkUvQq33HHXg/edit?usp=sharing" },
+  { nie: "20176661", nombre: "Corvera Alfaro, Gustavo Ezequiel",       grado: "8° Grado A", nacimiento: "12/03/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/19RB3F5fyoM-aC1nfzHfnwktJsJpe9soD-JwLPobj-Zk/edit?usp=sharing" },
+  { nie: "20138869", nombre: "Corvera De Paz, Diego Benjamín",         grado: "8° Grado A", nacimiento: "27/08/2011", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1l0zxrK1FRZH58LroSmCzQKyFgVurXB31bEFdtQib0bs/edit?usp=sharing" },
+  { nie: "20176662", nombre: "Corvera Flores, Pedro Ernesto",          grado: "8° Grado A", nacimiento: "04/09/2011", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1-diiVG8J23WrWFEG8T77a78JPnbpXaU2dHtUBNXk_X8/edit?usp=sharing" },
+  { nie: "20176663", nombre: "Corvera Fuentes, Luis Rafael",           grado: "8° Grado A", nacimiento: "11/09/2011", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1cLo5v1vPKo2LSYLWU6gIes4zkm_Eadcgyu3RIZfoNZQ/edit?usp=sharing" },
+  { nie: "10065035", nombre: "Duran Gómez, Justin Leonel",             grado: "8° Grado A", nacimiento: "29/12/2011", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1cSkudtbtBxB4JQEgSETgj16Fv7aXaFU2dTM9k275hJk/edit?usp=sharing" },
+  { nie: "20176664", nombre: "González Corvera, Héctor Gabriel",       grado: "8° Grado A", nacimiento: "02/02/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1eKyo3s-TU7DoNyCOzwO-noR7wkdHF2XhcsvjPeQ3RWE/edit?usp=sharing" },
+  { nie: "20305579", nombre: "Henríquez Chirino, Estiven Alexander",   grado: "8° Grado A", nacimiento: "15/06/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1s6Kzjc8wJBaYEb7Tx0Wpgg_3f7tDiYS3gpPwG7Oyu-M/edit?usp=sharing" },
+  { nie: "20305581", nombre: "Manueles Pereira, Keily Yaritza",        grado: "8° Grado A", nacimiento: "05/07/2011", sexo: "F", demeritos_url: "https://docs.google.com/spreadsheets/d/1iGJwl0UfHsBOBXdhCckrEm5qos-u5vzV4MgEe7SgKgw/edit?usp=sharing" },
+  { nie: "20176667", nombre: "Martínez Flores, Gisela Lisbeth",        grado: "8° Grado A", nacimiento: "14/03/2012", sexo: "F", demeritos_url: "https://docs.google.com/spreadsheets/d/1bjqn30YeGA8eKbQQVX2FACw2_l8zMcpaB9OOOq_JK5A/edit?usp=sharing" },
   { nie: "20176668", nombre: "Martínez Lazo, Ederson Osiel",           grado: "8° Grado A", nacimiento: "05/12/2011", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/19FjOkTHzszbyPd8IyLzj40gaVPfXf7zrdoJRsZfaGxQ/edit?usp=sharing" },
-  { nie: "20179890", nombre: "Mejía Carbajal, Ezequiel Vladimir",      grado: "8° Grado A", nacimiento: "31/07/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20179891", nombre: "Meléndez Corvera, Raúl Ernesto",         grado: "8° Grado A", nacimiento: "22/06/2012", sexo: "M", demeritos_url: "" },
-  { nie: "20293665", nombre: "Pérez Barahona, Katherine Alejandra",    grado: "8° Grado A", nacimiento: "05/04/2011", sexo: "F", demeritos_url: "" },
-  { nie: "20305583", nombre: "Rivas López, Samuel Josué",              grado: "8° Grado A", nacimiento: "15/06/2012", sexo: "M" }
+  { nie: "20179890", nombre: "Mejía Carbajal, Ezequiel Vladimir",      grado: "8° Grado A", nacimiento: "31/07/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1ig1GiItsLmiPkZJ_c0ixJD6oh2uGK3VJvn-rnhrdAT4/edit?usp=sharing" },
+  { nie: "20179891", nombre: "Meléndez Corvera, Raúl Ernesto",         grado: "8° Grado A", nacimiento: "22/06/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1T6QnAlCq81W5WP8qRLnMiMGagM9G22K9YIaaGeFS2VY/edit?usp=sharing" },
+  { nie: "20293665", nombre: "Pérez Barahona, Katherine Alejandra",    grado: "8° Grado A", nacimiento: "05/04/2011", sexo: "F", demeritos_url: "https://docs.google.com/spreadsheets/d/1HSPkUqUN5HEbjfX-BRvDR1O0C2JZSlhblMhFCfl6xIs/edit?usp=sharing" },
+  { nie: "20305583", nombre: "Rivas López, Samuel Josué",              grado: "8° Grado A", nacimiento: "15/06/2012", sexo: "M", demeritos_url: "https://docs.google.com/spreadsheets/d/1T3etjRkLbHplViW9p9SB_ivZXHZ3oZIe4MS6J9bLkWs/edit?usp=sharing" }
 );
