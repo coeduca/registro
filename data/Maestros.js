67bd42df408a133d04e707c86a0b41aa903a98eb
coeduca <coeducaanimas@gmail.com>
2026-03-26 21:55:33 -0600

    Add files via upload

---
+++ data/Maestros.js
@@ -0,0 +1,6 @@
+// ============================================================
+// Maestros
+// ============================================================
+(window.STUDENTS = window.STUDENTS || []).push(
+  { nie: "ellyxeo", nombre: "👨🏻‍🏫 Prof. Eliseo", grado: "Maestros", nacimiento: "05/04/1999", sexo: "M", demeritos_url: "" }
+);
